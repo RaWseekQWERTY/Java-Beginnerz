f0e71aefc8ee3a1a91966b35accc0487bd77ef25
RaWseekQWERTY <110769947+RaWseekQWERTY@users.noreply.github.com>
2022-11-10 17:06:30 +0545

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner; //importing scanner function
 public class Addition
 {
-    public static void main(String [] args)
+    public static void main(String [] args)  //main method
 {
-    int x,y,sum;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the value of x:");
+    int x,y,sum; //variables declaration
+    Scanner sc=new Scanner(System.in);//defining function
+    System.out.println("Enter the value of x:");//asking user for the value of x
      x=sc.nextInt();
      System.out.println("Enter the value of y:");
      y=sc.nextInt();
-    sum= sum(x,y);
-    System.out.println("The sum is:"+sum);
+    sum= sum(x,y);//return function
+    System.out.println("The sum is:"+sum);//result
 }
-public static int sum(int a, int b)
+public static int sum(int a, int b)//finding the sum
 {
     int sum=a+b;
     return sum;
