4f92be424d9f6d3864ec4975075be8a3e24804be
RaWseekQWERTY <rasikkayasthav@gmail.com>
2022-11-24 11:31:23 +0545

    Second message

---
+++ comparision.java
@@ -0,0 +1,14 @@
+public class comparision{
+    static int FirstNumber=50;
+    static int SecondNumber=60;
+ public static void main(String[] args){
+     if(FirstNumber==SecondNumber)
+         System.out.println("Both are equal");
+    else if(FirstNumber<SecondNumber)
+         System.out.println("SecondNumber is greater");
+    else
+     System.out.println("FirstNumber is greater");
+     
+     
+ }
+}
